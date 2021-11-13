35336ced29f5b9ca6a29841c1c99bbc0616da3e2
Tareq Hasan <tarreeq@gmail.com>
2021-11-13 16:24:16 +0200

    Implement basic tickets view with add and edit functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Tickets from './components/Tickets'
 
 function App() {
   return (
-    <div className="App">
-      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div class="flex-shrink-0">
-          <img class="h-12 w-12" src={logo} alt="ChitChat Logo" />
-        </div>
-        <div>
-          <div class="text-xl font-medium text-black">ChitChat</div>
-          <p class="text-gray-500">You have a new message!</p>
-        </div>
-      </div>
-    </div>
+    <Tickets />
   );
 }
 
---
+++ src/components/Tickets.js
@@ -0,0 +1,299 @@
+import { Fragment, useState, useEffect } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { v4 as uuidv4 } from 'uuid'
+import TicketsList from './TicketsList'
+import Pagination from './Pagination'
+import AddTicketForm from './AddTicketForm'
+
+import { default as data } from '../data.json'
+
+const user = {
+  name: 'Tareq Hasan',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://i.ibb.co/QPJ6Zj4/av-tq-1.png',
+}
+const navigation = [
+  { name: 'Tickets', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+  { name: 'Reports', href: '#', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Tickets() {
+  let pageSize = 10
+  const [offset, setOffset] = useState(0)
+  const [tickets, setTickets] = useState([])
+  const [ticketsPage, setTicketsPage] = useState(tickets.slice(offset, pageSize))
+  const [addTicketFormOpen, setAddTicketFormOpen] = useState(false)
+  const [editItem, setEditItem] = useState(null)
+  const pages = tickets.length / pageSize
+
+  useEffect(() => {
+    setTickets(data.tickets)
+  }, [])
+
+  useEffect(() => {
+    setTicketsPage(tickets.slice(offset, offset + pageSize))
+  }, [offset, pageSize, tickets])
+
+
+  const handleNextPage = () => {
+    if(offset + pageSize < tickets.length) {
+      setOffset(offset + pageSize)
+    }
+  }
+ 
+  const handlePreviousPage = () => {
+    if(offset > 0) {
+      setOffset(offset - pageSize)
+    }
+  }
+
+  const handleAddOrUpdateTicket = (subject, priority, ticketStatus, description) => {
+    // If edit mode
+    if(editItem) {
+      // get index of item to be updated
+      const updateIndex = tickets.findIndex(ticket => ticket.id === editItem.id)
+      setTickets([
+        ...tickets.slice(0, updateIndex),
+        {
+          id: editItem.id,
+          subject,
+          priority,
+          status: ticketStatus,
+          description
+        },
+        ...tickets.slice(updateIndex + 1)
+      ])
+      setAddTicketFormOpen(false)
+    } else {
+      // Save new ticket
+      setTickets([{
+        id: uuidv4(), 
+        subject,
+        priority,
+        status: ticketStatus,
+        description
+  
+      }, ...tickets])
+      setOffset(0)
+      setAddTicketFormOpen(false)
+    }
+    
+  }
+
+  const handleEditTicket = (id) => {
+    const editItemData = tickets.find(ticket => ticket.id === id)
+    setEditItem(editItemData)
+    setAddTicketFormOpen(true)
+  }
+
+  return (
+    <>
+      <div className="min-h-full">
+        <Disclosure as="nav" className="bg-gray-800">
+          {({ open }) => (
+            <>
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="h-8 w-8"
+                        src="https://i.imgur.com/Vw6TcCx.png"
+                        alt="Workflow"
+                      />
+                    </div>
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex items-baseline space-x-4">
+                        {navigation.map((item) => (
+                          <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                              'px-3 py-2 rounded-md text-sm font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                          >
+                            {item.name}
+                          </a>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-4 flex items-center md:ml-6">
+                      <button
+                        type="button"
+                        className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      >
+                        <span className="sr-only">View notifications</span>
+                        <BellIcon className="h-6 w-6" aria-hidden="true" />
+                      </button>
+
+                      {/* Profile dropdown */}
+                      <Menu as="div" className="ml-3 relative">
+                        <div>
+                          <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                            <span className="sr-only">Open user menu</span>
+                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {userNavigation.map((item) => (
+                              <Menu.Item key={item.name}>
+                                {({ active }) => (
+                                  <a
+                                    href={item.href}
+                                    className={classNames(
+                                      active ? 'bg-gray-100' : '',
+                                      'block px-4 py-2 text-sm text-gray-700'
+                                    )}
+                                  >
+                                    {item.name}
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            ))}
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </div>
+                  <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="md:hidden">
+                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  {navigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'block px-3 py-2 rounded-md text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+                <div className="pt-4 pb-3 border-t border-gray-700">
+                  <div className="flex items-center px-5">
+                    <div className="flex-shrink-0">
+                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                    </div>
+                    <div className="ml-3">
+                      <div className="text-base font-medium leading-none text-white">{user.name}</div>
+                      <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                    </div>
+                    <button
+                      type="button"
+                      className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    >
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                  <div className="mt-3 px-2 space-y-1">
+                    {userNavigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <header className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Tickets</h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            {/* Conent */}
+            <div className="flex justify-end">
+            <button
+              type="submit"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none mr-4 md:mr-0"
+              onClick={() => setAddTicketFormOpen(true)}
+            >
+              Add Ticket
+            </button>
+            </div>
+            
+            <div className="px-4 py-6 sm:px-0">
+            
+              <TicketsList 
+                tickets={ticketsPage} 
+                handleEditTicket={handleEditTicket}
+              />
+              <Pagination 
+                pages={pages} 
+                offset={offset} 
+                pageSize={pageSize} 
+                length={tickets.length}
+                handleNextPage={handleNextPage}
+                handlePreviousPage={handlePreviousPage}
+              />
+            </div>
+            <AddTicketForm 
+              open={addTicketFormOpen} 
+              setOpen={setAddTicketFormOpen} 
+              handleAddTicket={handleAddOrUpdateTicket}
+              editItem={editItem}
+              setEditItem={setEditItem}
+            />
+            {/* Conent End */}
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
