0f4e5d919c83be0af9ff6075df2e3fedd9d4b989
Tareq Hasan <tarreeq@gmail.com>
2021-11-13 18:21:28 +0200

    Fix some Tailwind CSS config related to Netlify deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist: [
+      'bg-red-200',
+      'bg-green-200',
+      'bg-yellow-200'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
